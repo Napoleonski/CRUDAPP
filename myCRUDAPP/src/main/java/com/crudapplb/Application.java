681de234dbe0aed49dfbed61e7452f963d4e3b2e
linuxlite <hageghadi@gmail.com>
2023-08-18 17:02:22 +0300

    added ItemController

--- myCRUDAPP/src/main/java/com/crudapplb/Application.java
+++ myCRUDAPP/src/main/java/com/crudapplb/Application.java
@@ -3,7 +3,7 @@ package com.crudapplb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.crudapplb")
 
 public class Application {
     public static void main(String[] args) {
--- myCRUDAPP/src/main/java/com/crudapplb/ItemRepository.java
+++ myCRUDAPP/src/main/java/com/crudapplb/ItemRepository.java
@@ -1,6 +1,8 @@
 package com.crudapplb;
- 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
+
 
 public interface ItemRepository extends JpaRepository<Item, Long> {
 }
